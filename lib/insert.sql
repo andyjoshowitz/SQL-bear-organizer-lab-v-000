032843f5fc0560479bac054989c1aed101071ff6
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-03-05 17:09:08 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Baloo", 10, "M", "Black", "Cuddly", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Brother", 3, "M", "Brown", "Sweet", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabita", 8, "F", "Grizzly", "Kind", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Pooh", 2, "M", "Grizzly", "Kind", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "M", "Brown", "Angry", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "F", "Black", "Loud", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 4, "M", "Grizzly", "Loving", 1);
