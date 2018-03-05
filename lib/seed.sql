853735ffe88be53ce505b4a51462dae360811fae
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-03-05 17:14:41 +0000

    automatically backed up by learn

--- lib/seed.sql
+++ lib/seed.sql
@@ -1,9 +1,9 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
-(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
-(2,"Rowdy", 10, "M", "black", "intense", 1),
-(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
-(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
-(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
-(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
-(7,"Wendy", 6, "F", "Blue", "naive", 1),
-(8,null, 20, "M", "black", "aggressive", 0);
+("Baloo", 10, "M", "Black", "Cuddly", 0),
+("Brother", 3, "M", "Brown", "Sweet", 1),
+("Pooh", 2, "M", "Grizzly", "Kind", 1),
+("Sarah", 9, "F", "Brown", "Grumpy", 0),
+("Tanya", 6, "F", "Black", "Loud", 1),
+("Smokey", 4, "M", "Grizzly", "Dopey", 0),
+("Katie", 6, "F", "Polar", "Sleepy", 0),
+
